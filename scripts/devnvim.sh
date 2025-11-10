aed1942c1903db8da75ad3b4dca89df986ecdbeb
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2025-11-09 22:51:03 -0500

    init

---
+++ scripts/devnvim.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+docker compose up -d dev
+docker compose exec dev bash -lc '
+node --version;
+if ! command -v yarn >/dev/null; then npm install -g yarn; fi;
+yarn install;
+'
+docker compose exec dev bash -lc 'nvim .'
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { v4 as uuid } from "uuid";
+
+function main() {
+        console.log(`Hello world with uuid: ${uuid()} `);
+}
+
+main();
