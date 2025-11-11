4f8091cad1a7fb46cf1ce6c6cb132fdaf14e5dc0
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2025-11-10 23:36:03 -0500

    start adding biome support

--- scripts/devnvim.sh
+++ scripts/devnvim.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 docker compose up -d dev
-docker compose exec dev bash -lc '
-node --version;
-if ! command -v yarn >/dev/null; then npm install -g yarn; fi;
-yarn install;
+docker compose exec --user node dev bash -lc '
+    node --version;
+    if ! command -v yarn >/dev/null; then npm install -g yarn; fi;
+    yarn install;
 '
 docker compose exec dev bash -lc 'nvim .'
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { v4 as uuid } from "uuid";
 
 function main() {
-        console.log(`Hello world with uuid: ${uuid()} `);
+	const arr = [];
+
+	arr.forEach((el) => {
+		console.log(el);
+	});
+
+	console.log(`Hello world with uuid: ${uuid()} `);
 }
 
 main();
